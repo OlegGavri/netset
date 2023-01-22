306e1754a2a2fe3e5ab6b5c053abd846b170f1bd
Oleg <reffum@bk.ru>
2023-01-22 23:51:05 +0300

    Add CoordinatorLayout, Toolbar etc.

--- app/src/main/java/com/riggle/netset/MainActivity.kt
+++ app/src/main/java/com/riggle/netset/MainActivity.kt
@@ -2,13 +2,18 @@ package com.riggle.netset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.riggle.netset.ui.main.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        toolbar.title = "Net interfaces"
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
